ca073261587002cc0131d3b6abce51ea7a97cca0
rishithaannepu2003 <annepurishitha@gmail.com>
2024-08-22 22:06:48 +0530

    Updates

---
+++ precache-manifest.dce6944ee283a360b7f2aaa0c0ee66dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5259f0c987290b6a8d17d66801221c",
+    "url": "/rishithaannepu2003/expense-tracker/index.html"
+  },
+  {
+    "revision": "148a49b24ca8209f1810",
+    "url": "/rishithaannepu2003/expense-tracker/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "19000fc372c563afe3c2",
+    "url": "/rishithaannepu2003/expense-tracker/static/js/2.c1744642.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/rishithaannepu2003/expense-tracker/static/js/2.c1744642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148a49b24ca8209f1810",
+    "url": "/rishithaannepu2003/expense-tracker/static/js/main.ece00e52.chunk.js"
+  },
+  {
+    "revision": "d6a742fcc3e5b825d7c0",
+    "url": "/rishithaannepu2003/expense-tracker/static/js/runtime-main.70088f0f.js"
+  }
+]);
